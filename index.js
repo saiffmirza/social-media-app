e73168ef59d3a8221e0d26402713646f877dac87
saiffmirza <saiffmirza@gmail.com>
2021-01-22 23:50:07 -0500

    added login and register

--- index.js
+++ index.js
@@ -1,36 +1,12 @@
 //dependancy imports
 const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
 //relative imports
-const Post = require('./models/Post');
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers')
 const { MONGODB } = require('./config.js'); 
 
-const typeDefs = gql`
-    type Post{
-        id: ID!
-        body: String!
-        createdAt: String!
-        username: String!
-
-    }
-    type Query {
-        getPosts: [Post]
-    }
-`
-const resolvers = {
-    Query: {
-        async getPosts(){
-            try{
-                const posts = await Post.find();
-                return posts;
-            }catch(err){
-                throw new Error(err);
-            }
-        }
-    }
-}
 
 const server = new ApolloServer({
     typeDefs,
